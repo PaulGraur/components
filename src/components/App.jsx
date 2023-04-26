9b907f3f9391293dfe3d77fcb533f84994d3af47
Paul <pawel.graur47@gmail.com>
2023-04-26 10:15:54 +0300

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import { Profile } from '../components/Profile/Profile';
+// import { Profile } from '../components/Profile/Profile';
 import { Statistics } from '../components/Statistics/Statistics';
 import { FriendList } from '../components/FriendList/FriendList';
 import { FriendListItem } from '../components/FriendList/FriendListItem';
 import { TransactionHistory } from '../components/TransactionHistory/TransactionHistory';
 
-import user from '../data/user.json';
+// import user from '../data/user.json';
 import stat from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
@@ -12,13 +12,13 @@ import transactions from '../data/transactions.json';
 export const App = () => {
   return (
     <div>
-      <Profile
+      {/* <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
 
       <Statistics stats={stat} />
 
